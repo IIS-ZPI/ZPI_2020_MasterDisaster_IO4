824ff94c1e915fb7749db6e276904eb7b0ff1e26
wojceichciejka <wojciech.ciejka@icloud.com>
2020-03-18 19:55:21 +0100

    Zad2 Lab1 gitID - wojciechciejka

--- Calculator/src/Zad1.java
+++ Calculator/src/Zad1.java
@@ -1,5 +1,6 @@
 public class Zad1 {
     public static void main(String[] argv) {
         System.out.println("MasterDisaster Operator GithubID: kamsykula");
+        System.out.println("MasterDisaster Developer GithubID: wojciechciejka");
     }
 }
