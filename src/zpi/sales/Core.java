59f6fd9dd776e7ca3b14ea0ca6ce72d11a2b970e
Asmei <kamsykula@gmail.com>
2020-04-01 17:49:30 +0200

    Initial commit for new project

---
+++ src/zpi/sales/Core.java
@@ -0,0 +1,10 @@
+package zpi.sales;
+
+import io.javalin.Javalin;
+
+public class Core {
+	public static void main(String[] args) {
+		Javalin app = Javalin.create().start(7000);
+		app.get("/", ctx -> ctx.result("Hello World"));
+	}
+}
