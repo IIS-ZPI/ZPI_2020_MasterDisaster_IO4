5e4d7c018db9519b0d86717f60ea76a2d8b50b60
Alicja <Krautsztrung>
2020-04-03 10:16:01 +0200

    Basic get/post app

--- src/main/java/zpi/sales/Core.java
+++ src/main/java/zpi/sales/Core.java
@@ -2,9 +2,23 @@ package zpi.sales;
 
 import io.javalin.Javalin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Core {
+	static Map<String, String> reservations = new HashMap<String, String>() ;
 	public static void main(String[] args) {
-		Javalin app = Javalin.create().start(7000);
-		app.get("/", ctx -> ctx.result("Hello World"));
+		Javalin app = Javalin.create(config -> {
+			config.addStaticFiles("/src/main/public");
+		}).start(7777);
+
+		app.post("/choose-state", ctx -> {
+			reservations.put(ctx.formParam("state"), ctx.formParam("category"));
+			ctx.html("You've choosen the state");
+		});
+
+		app.get("/check-taxes", ctx -> {
+			ctx.html(reservations.get(ctx.queryParam("state")));
+		});
 	}
 }
