4870d3b7e5a83db2826719535201c36da0c0be2e
Asmei <kamsykula@gmail.com>
2020-04-06 23:47:32 +0200

    Implemeted function to compute final price of product

--- src/main/java/zpi/sales/USState.java
+++ src/main/java/zpi/sales/USState.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 
 public class USState {
+	class NotFoundTaxForThisCategory extends Exception{}
+	
 	private String name;
 	private HashMap<Category, Double> taxForCategoryMap;
 	
@@ -19,5 +21,14 @@ public class USState {
 	
 	public void addCategoryWithTax(Category category, Double tax) {
 		this.taxForCategoryMap.put(category, tax);
+	}
+	
+	public Double computeFinalPriceOfProduct(Product product) throws NotFoundTaxForThisCategory {
+		if(!this.taxForCategoryMap.containsKey(product.getCategory())){
+			throw new NotFoundTaxForThisCategory();
+		}
+		Double ratio = this.taxForCategoryMap.get(product.getCategory());
+		
+		return ratio * product.getBasePrice() + product.getBasePrice();
 	}
 }
