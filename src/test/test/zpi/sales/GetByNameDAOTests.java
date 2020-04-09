4f24240673ac68d990171f1872e0efb721ed911f
belka <izaa1109@gmail.com>
2020-04-09 17:10:36 +0200

    + added tests for CategoryDAO

--- src/test/test/zpi/sales/GetByNameDAOTests.java
+++ src/test/test/zpi/sales/GetByNameDAOTests.java
@@ -5,6 +5,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import zpi.product.Product;
 import zpi.product.ProductDAO;
+import zpi.sales.Category;
+import zpi.sales.CategoryDAO;
 import zpi.state.USState;
 import zpi.state.USStateDAO;
 
@@ -16,14 +18,17 @@ public class GetByNameDAOTests {
 
     private static USStateDAO stateDAO = USStateDAO.getInstance();
     private static ProductDAO productDAO = ProductDAO.getInstance();
+    private static CategoryDAO categoryDAO = CategoryDAO.getInstance();
+
     private static USState state = new USState(NAME_THAT_EXISTS);
     private static Product product = new Product(NAME_THAT_EXISTS);
-
+    private static Category category = new Category(NAME_THAT_EXISTS);
 
     @BeforeClass
     public static void init() {
         stateDAO.getStates().add(state);
         productDAO.getProducts().add(product);
+        categoryDAO.getCategories().add(category);
     }
 
     @Test
@@ -35,19 +40,19 @@ public class GetByNameDAOTests {
     @Test
     public void checkIfStateNameNotExists(){
         Optional<USState> state = stateDAO.getUSStateByName(NAME_THAT_NOT_EXISTS);
-        Assert.assertEquals(null, state.orElse(null));
+        Assert.assertNull(state.orElse(null));
     }
 
     @Test
     public void checkIfStateNameIsNull(){
         Optional<USState> state = stateDAO.getUSStateByName(null);
-        Assert.assertEquals(null, state.orElse(null));
+        Assert.assertNull(state.orElse(null));
     }
 
     @Test
     public void checkIfStateNameIsEmpty(){
         Optional<USState> state = stateDAO.getUSStateByName("");
-        Assert.assertEquals(null, state.orElse(null));
+        Assert.assertNull(state.orElse(null));
     }
 
     @Test
@@ -59,18 +64,42 @@ public class GetByNameDAOTests {
     @Test
     public void checkIfProductNameNotExists(){
         Optional<Product> product = productDAO.getProductByName(NAME_THAT_NOT_EXISTS);
-        Assert.assertEquals(null, product.orElse(null));
+        Assert.assertNull(product.orElse(null));
     }
 
     @Test
     public void checkIfProductNameIsNull(){
         Optional<Product> product = productDAO.getProductByName(null);
-        Assert.assertEquals(null, product.orElse(null));
+        Assert.assertNull(product.orElse(null));
     }
 
     @Test
     public void checkIfProductNameIsEmpty() {
         Optional<Product> product = productDAO.getProductByName("");
-        Assert.assertEquals(null, product.orElse(null));
+        Assert.assertNull(product.orElse(null));
+    }
+
+    @Test
+    public void checkIfCategoryNameExists(){
+        Optional<Category> category = categoryDAO.getCategoryByName(NAME_THAT_EXISTS);
+        Assert.assertEquals(GetByNameDAOTests.category, category.orElse(null));
+    }
+
+    @Test
+    public void checkIfCategoryNameNotExists(){
+        Optional<Category> category = categoryDAO.getCategoryByName(NAME_THAT_NOT_EXISTS);
+        Assert.assertNull(category.orElse(null));
+    }
+
+    @Test
+    public void checkIfCategoryNameIsNull(){
+        Optional<Category> category = categoryDAO.getCategoryByName(null);
+        Assert.assertNull(category.orElse(null));
+    }
+
+    @Test
+    public void checkIfCategoryNameIsEmpty() {
+        Optional<Category> category = categoryDAO.getCategoryByName("");
+        Assert.assertNull(category.orElse(null));
     }
 }
