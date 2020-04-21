bb2899037789473ce1ee138f61afe63a71f7d2dd
belka <izaa1109@gmail.com>
2020-04-21 16:29:55 +0200

    add mock to usstateDao interface

--- src/test/test/zpi/sales/GetByNameDAOTests.java
+++ src/test/test/zpi/sales/GetByNameDAOTests.java
@@ -3,20 +3,25 @@ package test.zpi.sales;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import zpi.dao.DAOFactory;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import zpi.product.Product;
 import zpi.product.ProductDAO;
 import zpi.state.IUSStateDAO;
 import zpi.state.USState;
-import zpi.state.SimpleUSStateDAO;
+
 
 import java.util.Optional;
 
+@RunWith(MockitoJUnitRunner.class)
 public class GetByNameDAOTests {
     private static final String NAME_THAT_EXISTS = "NameThatExistsInData";
     private static final String NAME_THAT_NOT_EXISTS = "NameThatNotExistsInData";
 
-    private static IUSStateDAO stateDAO = new SimpleUSStateDAO();
+    @Mock
+    private static IUSStateDAO stateDAO = Mockito.mock(IUSStateDAO.class);
     private static ProductDAO productDAO = ProductDAO.getInstance();
 
     private static USState state = new USState(NAME_THAT_EXISTS);
@@ -30,24 +35,28 @@ public class GetByNameDAOTests {
 
     @Test
     public void checkIfStateNameExists(){
+        Mockito.when(stateDAO.getUSStateByName(NAME_THAT_EXISTS)).thenReturn(Optional.of(GetByNameDAOTests.state));
         Optional<USState> state = stateDAO.getUSStateByName(NAME_THAT_EXISTS);
         Assert.assertEquals(GetByNameDAOTests.state, state.orElse(null));
     }
 
     @Test
     public void checkIfStateNameNotExists(){
+        Mockito.when(stateDAO.getUSStateByName(NAME_THAT_NOT_EXISTS)).thenReturn(Optional.empty());
         Optional<USState> state = stateDAO.getUSStateByName(NAME_THAT_NOT_EXISTS);
         Assert.assertNull(state.orElse(null));
     }
 
     @Test
     public void checkIfStateNameIsNull(){
+        Mockito.when(stateDAO.getUSStateByName(null)).thenReturn(Optional.empty());
         Optional<USState> state = stateDAO.getUSStateByName(null);
         Assert.assertNull(state.orElse(null));
     }
 
     @Test
     public void checkIfStateNameIsEmpty(){
+        Mockito.when(stateDAO.getUSStateByName("")).thenReturn(Optional.empty());
         Optional<USState> state = stateDAO.getUSStateByName("");
         Assert.assertNull(state.orElse(null));
     }
