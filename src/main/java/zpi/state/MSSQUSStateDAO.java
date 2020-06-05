6224ee597e2c72dd0bf59072bc5a06e747b3d4ca
Asmei <kamsykula@gmail.com>
2020-06-05 12:04:29 +0200

    Speed up states getting

--- src/main/java/zpi/state/MSSQUSStateDAO.java
+++ src/main/java/zpi/state/MSSQUSStateDAO.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 public class MSSQUSStateDAO extends IUSStateDAO {
 	private final Database database;
+	List<USState> states = new LinkedList<>();
 	
 	public MSSQUSStateDAO(Database database) {
 		super();
@@ -20,21 +21,25 @@ public class MSSQUSStateDAO extends IUSStateDAO {
 	
 	@Override
 	public List<USState> getStates() {
-		List<USState> states = new LinkedList<>();
-		
-		final String selectSql = "SELECT id, stateName FROM States;";
-		
-		try (Statement statement = database.createStatement();
-		     ResultSet resultSet = statement.executeQuery(selectSql)) {
+		if (this.states.isEmpty()) {
+			List<USState> states = new LinkedList<>();
 			
-			while (resultSet.next()) {
-				USState state = new USState(resultSet.getString("stateName"));
-				selectTaxesForState(state);
-				states.add(state);
+			final String selectSql = "SELECT id, stateName FROM States;";
+			
+			try (Statement statement = database.createStatement();
+			     ResultSet resultSet = statement.executeQuery(selectSql)) {
+				
+				while (resultSet.next()) {
+					USState state = new USState(resultSet.getString("stateName"));
+					selectTaxesForState(state);
+					states.add(state);
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
 			
-		} catch (SQLException e) {
-			e.printStackTrace();
+			this.states = states;
 		}
 		
 		return states;
@@ -43,25 +48,31 @@ public class MSSQUSStateDAO extends IUSStateDAO {
 	
 	@Override
 	public Optional<USState> getUSStateByName(String name) {
-		USState state = null;
-		final String selectSql = String.format("SELECT id, stateName FROM States WHERE stateName='%s';", name);
-		
-		try (Statement statement = database.createStatement();
-		     ResultSet resultSet = statement.executeQuery(selectSql)) {
+		Optional<USState> state = Optional.empty();
+		if (this.states.isEmpty()) {
+			final String selectSql = String.format("SELECT id, stateName FROM States WHERE stateName='%s';", name);
 			
-			while (resultSet.next()) {
-				state = new USState(resultSet.getString("stateName"));
-				selectTaxesForState(state);
+			try (Statement statement = database.createStatement();
+			     ResultSet resultSet = statement.executeQuery(selectSql)) {
+				
+				while (resultSet.next()) {
+					state = Optional.of(new USState(resultSet.getString("stateName")));
+					selectTaxesForState(state.get());
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} else {
+			state = this.states.stream().filter(s->s.getName().equals(name)).findFirst();
 		}
-		return state == null ? Optional.empty() : Optional.of(state);
+		return state;
 	}
 	
 	@Override
 	public void editCategoryBaseTax(USState state, Category category, Double taxRatio) {
+		this.states.clear();
+		
 		final String updateTax = String.format("UPDATE Taxes SET tax=%f WHERE stateID=(SELECT id FROM States WHERE stateName='%s') and categoryID=(SELECT id FROM Categories WHERE category='%s');",
 				taxRatio, state.getName(), category);
 		
@@ -74,6 +85,8 @@ public class MSSQUSStateDAO extends IUSStateDAO {
 	
 	@Override
 	public void editCategoryValueWithoutTax(USState state, Category category, Double valueWithoutTax) {
+		this.states.clear();
+
 		final String updateTax = String.format("UPDATE Taxes SET valueWithoutTax=%f WHERE stateID=(SELECT id FROM States WHERE stateName='%s') and categoryID=(SELECT id FROM Categories WHERE category='%s');",
 				valueWithoutTax, state.getName(), category);
 		
